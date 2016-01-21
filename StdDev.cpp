e33e6e247331a2684e2e7ddf58a3a3686f152058
Andrew Niklas <acniklas@cpp.edu>
2016-01-20 22:39:48 -0800

    first attempt

--- StdDev.cpp
+++ StdDev.cpp
@@ -3,3 +3,44 @@
 // Completed 1/27/2016
 
 #include <iostream>
+#include <cmath>
+
+double sum(double&[], int);
+double mean(double&[], int);
+double stdDev(double&[], int);
+
+double values[10];
+int size = 10;
+double difSqrd[10];
+
+double sum(double& values[], int size){
+	double x = 0;
+	for(int i = 0; i < size; i++){
+		x += values[i];
+	}
+	return x;
+}
+
+double mean(double& values[], int size){
+	return sum(values, size)/size;
+}
+
+double stdDev(double& values[], int size){
+	double mean = mean(values, size);
+	for(int i = 0; i < size; i++){
+		difSqrd[i] = (values[i] - mean);
+	}
+	for(int i = 0; i < size; i++){
+		difSqrd[i] = pow(difSqrd[i], 2);
+	}
+	return sqrt(mean(difSqrd,size))
+}
+
+int main(){
+	for(int i = 0; i < size; i++){
+		std::cout << "input a double value: ";
+		std::cin >> values[i];
+	}
+	std::cout << "The Standard Deviation of the entered values is: ";
+	std::cout << stdDev(values, size) << std::endl;
+}
